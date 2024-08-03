f1a09aa7efca6ee205d1988f40b2b0d9daef73f9
MinuSingh1994 <kminusingh01@gmail.com>
2024-08-03 20:11:22 +0530

    changes her

---
+++ src/components/Tophead/Tophead.js
@@ -0,0 +1,23 @@
+//import  something  from "somewhere";
+import React from "react";
+import './Tophead.css';
+
+
+
+function Tophead() {
+  return (
+    <>
+     <div className="tophead">
+       <div className="left">Welcome Guest</div>
+       <div className="right">Stay Updated</div>
+
+
+       
+
+     </div>
+    
+    </>
+  )
+}
+
+export default Tophead
